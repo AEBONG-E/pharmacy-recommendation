5f9386d13077b4a8ade84016d82bbd22cd98a9be
hooney <hooney200@gmail.com>
2025-02-09 17:13:16 +0900

    first commit

---
+++ src/main/java/com/biz/fastcampus/pharmacyrecommendation/FastcampusPharmacyRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.biz.fastcampus.pharmacyrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusPharmacyRecommendationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusPharmacyRecommendationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/biz/fastcampus/pharmacyrecommendation/FastcampusPharmacyRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biz.fastcampus.pharmacyrecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusPharmacyRecommendationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
