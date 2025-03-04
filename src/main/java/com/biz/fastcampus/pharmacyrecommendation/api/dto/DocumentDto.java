2007eb526ce484675032007f8ecbd978efe649a4
hooney <hooney200@gmail.com>
2025-03-04 22:40:53 +0900

    Implementing Kakao Address Search API

---
+++ src/main/java/com/biz/fastcampus/pharmacyrecommendation/api/dto/DocumentDto.java
@@ -0,0 +1,21 @@
+package com.biz.fastcampus.pharmacyrecommendation.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DocumentDto {
+
+    @JsonProperty("address_name")
+    private String addressName; // 전체 지번 주소 또는 전체 도로명 주소, 입력에 따라 결정됨
+//    private String addressType; // address_name 값의 타입 (REGION: 지명, ROAD: 도로명, REGION_ADDR: 지번 주소, ROAD_ADDR: 도로명 주소
+    @JsonProperty("y")
+    private double latitude;           // y 좌표값, 위도
+    @JsonProperty("x")
+    private double longitude;          // x 좌표값, 경도
+
+}
