e84bff6c417ec848bbebb2942af8f38419f80290
hooney <hooney200@gmail.com>
2025-03-24 22:27:49 +0900

    Implementing a function to save pharmacy route recommendation results

---
+++ src/main/java/com/biz/fastcampus/pharmacyrecommendation/api/pharmacy/service/PharmacyRecommendationService.java
@@ -0,0 +1,41 @@
+package com.biz.fastcampus.pharmacyrecommendation.api.pharmacy.service;
+
+import com.biz.fastcampus.pharmacyrecommendation.api.dto.DocumentDto;
+import com.biz.fastcampus.pharmacyrecommendation.api.dto.KakaoApiResponseDto;
+import com.biz.fastcampus.pharmacyrecommendation.api.service.KakaoAddressSearchService;
+import com.biz.fastcampus.pharmacyrecommendation.direction.entity.Direction;
+import com.biz.fastcampus.pharmacyrecommendation.direction.service.DirectionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PharmacyRecommendationService {
+
+    private final KakaoAddressSearchService kakaoAddressSearchService;
+    private final DirectionService directionService;
+
+    public void recommendPharmacyList(String address) {
+
+        KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
+
+        if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
+            log.error("[PharmacyRecommendationService recommendPharmacyList fail] Input address: {}",  address);
+            return;
+        }
+
+        DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
+
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
+
+        directionService.saveAll(directionList); 
+
+    }
+
+}
---
+++ src/main/java/com/biz/fastcampus/pharmacyrecommendation/direction/repository/DirectionRepository.java
@@ -0,0 +1,8 @@
+package com.biz.fastcampus.pharmacyrecommendation.direction.repository;
+
+import com.biz.fastcampus.pharmacyrecommendation.direction.entity.Direction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DirectionRepository extends JpaRepository<Direction, Long> {
+
+}
